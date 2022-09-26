391d9233f70f1be556338d7e7d7bed76955ec1f1
Gözde SARPER <72733940+gozdesarper@users.noreply.github.com>
2022-09-26 17:47:39 +0300

    Update mortgageManager.java

--- creditManager/src/creditManager/mortgageManager.java
+++ creditManager/src/creditManager/mortgageManager.java
@@ -1,6 +1,6 @@
 package creditManager;
 
-public class mortgageManager extends creditManager{ // interface implements oluyor.
+public class MortgageManager extends CreditManager{ // interface implements oluyor.
 	public void calculate() {
 		System.out.println("Morgage kredi hesaplandı");
 }
